d4c670c00fd24e7cc47913c320bc39fba2c700d6
Gunjan Payal <gunjanpayal@icloud.com>
2023-11-07 13:37:45 +0530

    finished rmi implementation and added debug mode

--- Program.java
+++ Program.java
@@ -1,4 +1,5 @@
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 import storage.KeyValueStore;
 public class Program {
@@ -6,6 +7,13 @@ public class Program {
         int host_id = Integer.parseInt(args[0]);
         KeyValueStore kv_store = new KeyValueStore(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]),15000);
         kv_store.initialize_peers();
+        if(args.length==4 && args[3].equals("--debug")){
+            Scanner sc = new Scanner(System.in);
+            while(true){
+                String query = sc.nextLine();
+                execute_query(query,kv_store);
+            }
+        }
         if(host_id==0){
             kv_store.execute_put("10","asdf8asdf");
             kv_store.execute_put("11","asdfa");
@@ -26,5 +34,22 @@ public class Program {
             
         }
     }
-    
+    public static void execute_query(String query,KeyValueStore kv_store){
+        String[] query_terms = query.split("\\ ");
+        if(query_terms[0].equals("PUT")){
+            String key = query_terms[1];
+            String value = query_terms[2];
+            kv_store.execute_put(key, value);
+        } else if(query_terms[0].equals("GET")){
+            String key = query_terms[1];
+            kv_store.execute_get(key);
+        } else if(query_terms[0].equals("DELETE")){
+            String key = query_terms[1];
+            kv_store.execute_delete(key);
+        } else if(query_terms[0].equals("STORE")){
+            kv_store.execute_store();
+        } else {
+            System.out.println("Illegal Query!");
+        }
+    }
 }
