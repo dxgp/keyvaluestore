ae51150b5fe02936a068825454add6ffe807bcd8
Shrinivas Choudhary <shrini@Shrinivass-MacBook-Air.local>
2023-11-30 16:18:30 +0530

    put thread fix

--- threads/HandlePutThread.java
+++ threads/HandlePutThread.java
@@ -22,20 +22,23 @@ public class HandlePutThread implements Runnable {
     }
     public void run(){
         try{
-            // if(kv_store.host_id==2 || kv_store.host_id==1){
-            //     Thread.sleep(10000);
-            // }
-            if(kv_store.voted_on.containsKey(key)){
-                reply_socket.getOutputStream().write(("NO\n").getBytes());
-            } else{
-                kv_store.voted_on.put(key, true);
+            if(!kv_store.keys_random_pairs.containsKey(key)){
+                if(!(kv_store.local_store.containsKey(key) || kv_store.peer_table.containsKey(key))){
+                    reply_socket.getOutputStream().write(("YES\n").getBytes());
+                } else{
+                    reply_socket.getOutputStream().write(("NO\n").getBytes());
+                }
+                return;
             }
+            // If the requested key is already taken by this node or some other node return NO
             if(kv_store.local_store.containsKey(key) || kv_store.peer_table.containsKey(key)){
                 reply_socket.getOutputStream().write(("NO\n").getBytes());
             } else{
+                // If current node is working on same key resolve clash by comparing random no.s
                 if(kv_store.keys_random_pairs.containsKey(key)){
                     int self_random = kv_store.keys_random_pairs.get(key);
-                    if(recvd_rand>self_random){
+                    System.err.println("COMPARING RANDOMS +++++++");
+                    if(recvd_rand > self_random){
                         reply_socket.getOutputStream().write(("YES\n").getBytes());
                     } else{
                         reply_socket.getOutputStream().write(("NO\n").getBytes());
@@ -44,7 +47,6 @@ public class HandlePutThread implements Runnable {
                     reply_socket.getOutputStream().write(("YES\n").getBytes());
                 }
             }
-            reply_socket.getOutputStream().flush();
-        } catch(Exception e){e.printStackTrace();}
+        } catch(Exception e){}
     }
 }
